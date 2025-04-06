276248f48881a6955ca0bb8363d5359c3787f86a
Eduard <tula71ya@gmail.com>
2025-04-06 21:51:07 +0300

    Добавил dbFile для загрузки, выгрузки данных

--- dbFile/writeFiles.py
+++ dbFile/writeFiles.py
@@ -4,7 +4,7 @@ def write_file(array, mode):
     file = open("notes.csv", mode = 'w', encoding='utf-8')
     file.seek(0)
     file.close()
-    file = open("notes.cdv", mode = mode, encoding='utf-8')
+    file = open("notes.csv", mode=mode, encoding='utf-8')
     for notes in array:
         file.write(model.note.note.to_string(notes))
         file.write('\n')
